886b1f401161d89e964ca59372446999267e182a
ArnaudBan <arnaud@matierenoire.io>
2020-12-28 09:24:19 +0100

    fix: Affichage des composteurs sous les medias

--- src/components/medias/MediasList.js
+++ src/components/medias/MediasList.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import { DeleteWithConfirmButton, ImageField, List } from 'react-admin'
-import { Grid } from '@material-ui/core'
+import { Grid, List as ListMat, ListItem, ListItemText } from '@material-ui/core'
 
 const MediaGrid = ({ ids, data, basePath }) => (
   <Grid container>
-    {ids.map(id => (
-      <Grid key={id} item xs={12} sm={6} md={3} style={{ marginBottom: '1rem' }}>
-        <ImageField record={data[id]} source="contentUrl" src="contentUrl" />
-        <DeleteWithConfirmButton resource="media_objects" basePath={basePath} record={data[id]} />
-      </Grid>
-    ))}
+    {ids.map((id) => {
+      const record = data[id]
+      return (
+        <Grid key={id} item xs={12} sm={6} md={3} style={{ marginBottom: '1rem' }}>
+          <ImageField record={record} source="contentUrl" src="contentUrl" />
+          {record.composters.length > 0 ? (
+            <ListMat dense>
+              {record.composters.map((c) => (
+                <ListItem>
+                  <ListItemText primary={c.name} />
+                </ListItem>
+              ))}
+            </ListMat>
+          ) : (
+            'Pas de composteur'
+          )}
+          <DeleteWithConfirmButton resource="media_objects" basePath={basePath} record={record} />
+        </Grid>
+      )
+    })}
   </Grid>
 )
 
-const MediasList = props => (
+const MediasList = (props) => (
   <List title="Les images" {...props} exporter={false} perPage={25} sort={{ field: 'id', order: 'DESC' }}>
     <MediaGrid />
   </List>
